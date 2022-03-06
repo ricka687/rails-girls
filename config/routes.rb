20c880baffec8a368680d65f7e568419e6962107
your-name <your-email>
2022-03-06 13:21:29 +0900

    type your commit message here

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  root to: redirect('/ideas')
   get 'pages/info'
   resources :ideas
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get "pages/info"
-  root to: redirect('/ideas')
 
 end
